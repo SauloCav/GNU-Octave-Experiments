e689bc12812a200d9da7a58696db08029cbdf97d
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2022-02-07 20:18:38 -0300

    saturation filter is ready

---
+++ saturation_filter.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import skimage
+import skimage.feature
+import skimage.viewer
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+from PIL import ImageEnhance
+
+img = PIL.Image.open('the-good-the-bad-and-the-ugly_764d_1920x1080.jpg')
+
+converter = ImageEnhance.Color(img)
+
+img3 = converter.enhance(2)
+
+plt.imshow(img3)
+plt.show()
